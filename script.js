c43a0b9fa118360f5f16d1a2894af474ee35ff91
Gulnaz Khafizova <gulnazkhafizovaa@gmail.com>
2024-11-26 10:46:58 +0300

    Added changes - moving pictures on the main page

--- script.js
+++ script.js
@@ -14,15 +14,3 @@ document.querySelector('.carousel-container').addEventListener('mouseover', () =
 document.querySelector('.carousel-container').addEventListener('mouseout', () => {
   clearInterval(interval);
 });
-
-// добавь то что ниже в файл main 
-<div class="carousel-container">
-            <img src="./background.jpg" alt="Static Image" class="static-image">
-            <div class="carousel">
-              <a href="order-form1.html"><img src="fresh_air.jpg" alt="одуванчики"></a>
-              <a href="order-form2.html"><img src="honey_fields_gift.jpg" alt="полевыецветы"></a>
-              <a href="order-form3.html"><img src="horses.jpeg" alt="лошади"></a>
-              <a href="order-form3.html"><img src="roses.jpeg" alt="розы"></a>
-            </div>
-        </div>
-        <script src="script.js"></script> 
